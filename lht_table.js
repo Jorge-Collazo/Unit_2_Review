5e6543b71830bd38889a03ba40d783caa72d605b
Jorge Collazo <jcollazo151@west-mec.org>
2020-02-03 14:51:21 -0700

    Finished the Review

---
+++ lht_table.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Review Assignment
+
+   Author: Jorge Collazo
+   Date:   2-3-20
+
+	
+*/
+
+//Global variables
+var thisDay = new Date("August 30, 2018");
+var tableHTML = "<table id='eventTable'><caption>Upcoming Events</caption><tr><th>Date</th><th>Event</th><th>Price</th></tr>";
+var endDate = new Date(thisDay.getTime()+14*24*60*60*1000);
+//
+for(var i=0; i < eventDates.length; i++){
+   var eventDate = new Date(eventDates[i]);
+   var eventDay = eventDate.toDateString();
+   var eventTime = eventDate.toLocaleTimeString();
+
+   if(thisDay <= eventDate && eventDate <= endDate){
+   tableHTML += "<tr> <td>" + eventDay + " @ " + eventTime + "</td> <td>" + eventDescriptions[i] + "</td> <td>" + eventPrices[i] + "</td> </tr>";
+}}
+
+tableHTML += "</table>";
+
+document.getElementById("eventList").innerHTML = tableHTML;
